b621aa59fa5f18476d403e1b19430548f59b9f60
stamakunov7 <147558914+stamakunov7@users.noreply.github.com>
2026-02-22 01:19:32 -0500

    implemented uncomplete function in main.go

--- cli_task_manager.go
+++ cli_task_manager.go
@@ -64,6 +64,23 @@ func main() {
 		}
 		println("Task completed successfully.")
 
+	case "uncomplete":
+		if len(args) < 2 {
+			println("Please provide the ID of the task to complete.")
+			return
+		}
+		taskID, err := strconv.Atoi(args[1])
+		if err != nil {
+			println("Invalid task ID:", args[1])
+			return
+		}
+		tasks, err = cmdUncomplete(tasks, taskID)
+		if err != nil {
+			println("Error uncompleting task:", err.Error())
+			return
+		}
+		println("Task uncompleted successfully.")
+
 	case "edit":
 		if len(args) < 3 {
 			println("Please provide the ID and Description of the task to edit.")
